b98b0508856fe8eb2f745e4a5586fac92b74e001
ggbbest <www.ggb.best@gmail.com>
2022-01-16 10:37:20 +0900

    cro change

--- src/constants/c4eiToken.ts
+++ src/constants/c4eiToken.ts
@@ -4,8 +4,8 @@ export const C4EI_ADDRESS = process.env.REACT_APP_C4EI_TOKEN_ADDRESS
 if (typeof C4EI_ADDRESS === 'undefined') throw new Error('C4EI address is not configured')
 
 const C4EI_TOKEN =
-  process.env.REACT_APP_CHAIN_ID === String(ChainId.MAINNET)
-    ? new Token(ChainId.MAINNET, C4EI_ADDRESS, 18, 'C4EI', 'c4ei.net Coin')
-    : new Token(ChainId.ROPSTEN, C4EI_ADDRESS, 18, 'C4EI', 'c4ei.net Coin')
-
+  // process.env.REACT_APP_CHAIN_ID === String(ChainId.MAINNET)
+  //   ? new Token(ChainId.MAINNET, C4EI_ADDRESS, 18, 'C4EI', 'c4ei.net Coin')
+  //   : new Token(ChainId.ROPSTEN, C4EI_ADDRESS, 18, 'C4EI', 'c4ei.net Coin')
+  new Token(ChainId.C4EI, C4EI_ADDRESS, 18, 'C4EI', 'c4ei.net Coin')
 export default C4EI_TOKEN
